35ec7ecf678d8cbb092d68dfb522f7c122a7b737
Joseph Baffoe <joe.baffoe@gmail.com>
2021-10-23 13:45:47 -0600

    Baseline JSON

--- TrainProject/ViewController.swift
+++ TrainProject/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func myButton(_ sender: UIButton)
+    {
+        let urlString = "https://tsimobile.viarail.ca/data/allData.json"
+
+        if let url = URL(string: urlString)
+        {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let err = error
+                {
+                    print(err)
+                    return
+                }
+
+                do
+                {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? [[String: Any]]
+                }catch let jsonError
+                {
+                    print(jsonError)
+                }
+
+            }.resume()
+            
+            
+        }
+    }
+    
+ 
 
 }
-
