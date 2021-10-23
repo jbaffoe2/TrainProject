7524947043c9537e083a22b61c6daf47e64f9953
Joseph Baffoe <joe.baffoe@gmail.com>
2021-10-23 13:27:52 -0600

    Initial Commit

---
+++ TrainProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainProject
+//
+//  Created by Baffoe, Joseph - FUT on 10/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
